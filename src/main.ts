c0c23b5c344bf2c5addbbc524b9f9a0cc9c9f041
Edmond Ó Floinn <oflynned@tcd.ie>
2021-07-13 23:12:35 +0200

    refactor: simplify main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,32 @@
-import * as core from '@actions/core';
-import { getReleaseVersion, Version } from './version';
+import { Toolkit } from 'actions-toolkit';
 
-const defaultGradleLocation = 'app/build.gradle';
+type Key = 'GRADLE-LOCATION' | 'TAG-PREFIX';
 
-async function run(): Promise<void> {
-  try {
-    const gradleLocation =
-      core.getInput('gradle-location') ?? defaultGradleLocation;
-    const buildGradleDirectory = `${process.env.GITHUB_WORKSPACE}/${gradleLocation}`;
+const getValue = (toolkit: Toolkit, key: Key, fallback?: string): string => {
+  return toolkit.inputs[`INPUT_${key}`] ?? fallback ?? '';
+};
 
-    const tagPrefix = core.getInput('tag-prefix') ?? '';
-    const skipCi = core.getInput('skip-ci') === 'true';
-    const commitMessage = core.getInput('commit-message');
+const getGradleLocation = (toolkit: Toolkit): string => {
+  return getValue(toolkit, 'GRADLE-LOCATION', 'app/build.gradle');
+};
 
-    const currentVersion: Version = {
-      major: 1,
-      minor: 0,
-      patch: 0,
-    };
-    const commits: string[] = [];
+const getTagPrefix = (toolkit: Toolkit): string => {
+  return getValue(toolkit, 'TAG-PREFIX');
+};
 
-    const { isReleasable, version } = getReleaseVersion(
-      commits,
-      currentVersion,
-    );
+const isSkippingCi = (toolkit: Toolkit): boolean => {
+  return toolkit.inputs['skip-ci'] === 'true';
+};
 
-    if (!isReleasable) {
-    }
+Toolkit.run(async (tools): Promise<void> => {
+  const gradleLocation = getGradleLocation(tools);
+  const tagPrefix = getTagPrefix(tools);
+  const skipCi = isSkippingCi(tools);
+  const commitMessage = tools.inputs['commit-message'];
 
-    //
-    // core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    //
-    // core.debug(new Date().toTimeString());
-    // await wait(parseInt(ms, 10));
-    // core.debug(new Date().toTimeString());
-    //
-    // core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+  const { commits } = tools.context.payload;
+
+  if (!commits) {
+    // no commits, bump patch version for ci?
   }
-}
-
-run();
+});
