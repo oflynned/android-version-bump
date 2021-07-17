d6635271c2540b3e52bc265d44467528a28e79a7
Edmond Ó Floinn <oflynned@tcd.ie>
2021-07-17 15:56:12 +0200

    chore: update main

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,50 @@
 import { Toolkit } from 'actions-toolkit';
-
-type Key = 'GRADLE-LOCATION' | 'TAG-PREFIX';
-
-const getValue = (toolkit: Toolkit, key: Key, fallback?: string): string => {
-  return toolkit.inputs[`INPUT_${key}`] ?? fallback ?? '';
-};
-
-const getGradleLocation = (toolkit: Toolkit): string => {
-  return getValue(toolkit, 'GRADLE-LOCATION', 'app/build.gradle');
-};
-
-const getTagPrefix = (toolkit: Toolkit): string => {
-  return getValue(toolkit, 'TAG-PREFIX');
-};
-
-const isSkippingCi = (toolkit: Toolkit): boolean => {
-  return toolkit.inputs['skip-ci'] === 'true';
-};
+import {
+  getCommitMessage,
+  getGradleLocation,
+  getTagPrefix,
+  isSkippingCi,
+} from './env';
+import { getReleaseVersion, Version } from './version';
 
 Toolkit.run(async (tools): Promise<void> => {
-  const gradleLocation = getGradleLocation(tools);
-  const tagPrefix = getTagPrefix(tools);
-  const skipCi = isSkippingCi(tools);
-  const commitMessage = tools.inputs['commit-message'];
+  const githubToken = process.env.GITHUB_TOKEN;
 
-  const { commits } = tools.context.payload;
+  if (!githubToken) {
+    tools.exit.failure('No GitHub token set in env, cannot continue run');
+    return;
+  }
 
-  if (!commits) {
-    // no commits, bump patch version for ci?
+  try {
+    const gradleLocation = getGradleLocation(tools);
+    const gradleFile = tools.readFile(gradleLocation);
+
+    console.log({ gradleFile });
+
+    const tagPrefix = getTagPrefix(tools);
+    const skipCi = isSkippingCi(tools);
+
+    // create version 0.0.0 by default in build.gradle
+
+    // fetch the current values
+    const fileVersion: Version = {
+      major: 1,
+      minor: 0,
+      patch: 0,
+    };
+
+    const { commits } = tools.context.payload;
+    const { current, next } = getReleaseVersion(commits ?? [], fileVersion);
+    const commitMessage = getCommitMessage(tools, next, tagPrefix, skipCi);
+
+    console.log({ commitMessage });
+    console.log({ next });
+
+    tools.exit.success(
+      `Version bumped from ${current.name} to ${next.name} successfully!`,
+    );
+  } catch (e) {
+    tools.log.fatal(e);
+    tools.exit.failure('Failed to bump version!');
   }
 });
