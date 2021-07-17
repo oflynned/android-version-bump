466609d394e6e52dbda022ccfc8e00e639dc5e0f
Edmond Ó Floinn <oflynned@tcd.ie>
2021-07-17 18:15:21 +0200

    chore: add full workflow to main

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { Toolkit } from 'actions-toolkit';
 import {
+  getBuildNumber,
   getCommitMessage,
-  getGradleLocation,
   getTagPrefix,
   isSkippingCi,
 } from './env';
-import { getReleaseVersion, Version } from './version';
+import { createCommit, pushChanges, setGitIdentity } from './git';
+import {
+  doesVersionPropertiesExist,
+  getVersionProperties,
+  setVersionProperties,
+} from './gradle';
+import { Build, bumpBuild, getBuildFromVersion, Version } from './version';
 
 Toolkit.run(async (tools): Promise<void> => {
   const githubToken = process.env.GITHUB_TOKEN;
@@ -16,33 +22,38 @@ Toolkit.run(async (tools): Promise<void> => {
   }
 
   try {
-    const gradleLocation = getGradleLocation(tools);
-    const gradleFile = tools.readFile(gradleLocation);
-
-    console.log({ gradleFile });
-
     const tagPrefix = getTagPrefix(tools);
     const skipCi = isSkippingCi(tools);
+    const buildNumber = getBuildNumber(tools);
 
-    // create version 0.0.0 by default in build.gradle
+    const versionFileExists = await doesVersionPropertiesExist(tools);
+    let build: Build;
 
-    // fetch the current values
-    const fileVersion: Version = {
-      major: 1,
-      minor: 0,
-      patch: 0,
-    };
+    if (versionFileExists) {
+      const existingVersion = await getVersionProperties(tools);
+      const { commits } = tools.context.payload;
 
-    const { commits } = tools.context.payload;
-    const { current, next } = getReleaseVersion(commits ?? [], fileVersion);
-    const commitMessage = getCommitMessage(tools, next, tagPrefix, skipCi);
+      build = bumpBuild(commits ?? [], existingVersion, buildNumber);
+    } else {
+      // create version 0.0.1 by default in build.gradle if not exists
+      const defaultBuild: Version = {
+        major: 0,
+        minor: 0,
+        patch: 1,
+        build: buildNumber,
+      };
 
-    console.log({ commitMessage });
-    console.log({ next });
+      build = getBuildFromVersion(defaultBuild);
+    }
 
-    tools.exit.success(
-      `Version bumped from ${current.name} to ${next.name} successfully!`,
-    );
+    const message = getCommitMessage(tools, build, tagPrefix, skipCi);
+
+    await setVersionProperties(tools, build.version);
+    await setGitIdentity(tools);
+    await createCommit(tools, message);
+    await pushChanges(tools, build.name);
+
+    tools.exit.success(`Version bumped from to ${build.name} successfully!`);
   } catch (e) {
     tools.log.fatal(e);
     tools.exit.failure('Failed to bump version!');
